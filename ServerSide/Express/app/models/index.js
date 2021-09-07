fe61740f474c67115caf444dcb75d6e987f23bea
Mark Christison <53839878+MCKevmeister@users.noreply.github.com>
2021-09-07 14:11:27 +1200

    add all initModels

--- ServerSide/Express/app/models/index.js
+++ ServerSide/Express/app/models/index.js
@@ -1,6 +1,7 @@
-const dbConfig = require("../config/db.config.js");
-
 const Sequelize = require("sequelize");
+const dbConfig = require("../config/db.config.js");
+const initModels = require("./init-models").initModels;
+
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
     host: dbConfig.HOST,
     dialect: dbConfig.dialect,
@@ -14,11 +15,21 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
     }
 });
 
+const models = initModels(sequelize);
+
 const db = {};
+
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.model = require("./product.model")(sequelize, Sequelize);
+db.category = models.category;
+db.customer = models.customer;
+db.employee = models.employee;
+db.order = models.order;
+db.order_product = models.order_product;
+db.payment = models.payment;
+db.person = models.person;
+db.product = models.product;
 
 module.exports = db;
