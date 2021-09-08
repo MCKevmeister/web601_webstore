72e1309db31034ee6b332163f31b4ef38eb6a192
Mark Christison <53839878+MCKevmeister@users.noreply.github.com>
2021-09-09 11:22:00 +1200

    create user routes

---
+++ ServerSide/Express/app/routes/user.routes.js
@@ -0,0 +1,21 @@
+module.exports = app => {
+    const user = require("../controlers/user.controller.js");
+    const router = require ( "express" ).Router();
+
+    // Retrieve all users
+    router.get("/", user.findAll);
+
+    // Retrieve a single user by id
+    router.get("/:id", user.findOne);
+
+    // Create a new user
+    router.post("/", user.create);
+
+    // Updates a user with id
+    router.put("/:id", user.update);
+
+    // Delete a user with id
+    router.delete("/:id", user.delete);
+
+    app.use('/api/product', router);
+};
